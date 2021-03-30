301a0ce79cc9d140ba02528d2eddcc44291529ad
LuyenDao <luyenddgc00774@gmail.com>
2021-03-30 18:53:35 +0700

    1111

---
+++ AlamofileDemo/AlamofileDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AlamofileDemo
+//
+//  Created by Boss on 9/5/19.
+//  Copyright © 2019 LuyệnĐào. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("123123")
+    }
+
+
+}
+
